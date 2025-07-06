ccd1b5cfdf36b0eaec011436b89cf001183b3263
VickY26834 <vickykumar8789058145@gmail.com>
2025-07-06 19:45:20 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,14 @@
 const hre = require("hardhat");
 
 async function main() {
-  const SubscriptionPlatform = await hre.ethers.getContractFactory("SubscriptionPlatform");
+  const SubscriptionPlatform = await hre.ethers.getContractFactory("LotteryPlatform");
 
   const subscriptionFeeInWei = hre.ethers.utils.parseEther("0.01"); // 0.01 ETH
   const contract = await SubscriptionPlatform.deploy(subscriptionFeeInWei);
 
   await contract.deployed();
 
-  console.log("SubscriptionPlatform deployed to:", contract.address);
+  console.log("LotteryPlatform deployed to:", contract.address);
 }
 
 main().catch((error) => {
